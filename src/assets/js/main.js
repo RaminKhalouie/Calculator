e4e0510a536413ecceb9555e596fa0c18b3bad47
Ramin Khalouie <ra.khalouie@gmail.com>
2024-12-12 11:22:00 +0330

    [ADD] : Complete Logic
    Add main.js and complete the logic

---
+++ src/assets/js/main.js
@@ -0,0 +1,98 @@
+const result = document.getElementById("result");
+const buttonList = document.querySelectorAll('button');
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+let flag = false;
+
+buttonList.forEach((el) => {
+    el.addEventListener('click', (e) => {
+        const data = el.dataset.value;
+        SetValue(data);
+        // if (data != 'clear') {
+        //     result.textContent = result.textContent + data;
+        // } else {
+        //     firstNumber = 0;
+        //     secondNumber = 0;
+        //     operator = '';
+        // }
+
+    });
+});
+
+function SetValue(val) {
+
+    switch (val) {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+            if (operator == null || operator == '') {
+                firstNumber += val;
+            } else {
+                secondNumber += val;
+            }
+            if (flag) {
+                result.textContent = val;
+                flag = false;
+            } else {
+                result.textContent += val;
+            }
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            result.textContent += val;
+            operator = val;
+            break;
+        case '=':
+            calc();
+            break;
+        case 'clear':
+            reset();
+            break;
+    }
+}
+
+function reset() {
+    result.textContent = '';
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = '';
+}
+
+function calc() {
+    let res;
+    switch (operator) {
+        case '+':
+            res = Number(firstNumber) + Number(secondNumber);
+            break;
+        case '-':
+            res = firstNumber - secondNumber;
+            break;
+        case '*':
+            res = firstNumber * secondNumber;
+            break;
+        case '/':
+            if (secondNumber == 0) {
+                alert("Number cannot diveded by zero");
+            } else {
+                res = firstNumber / secondNumber;
+            }
+            break;
+    }
+    result.textContent = res;
+
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = '';
+    flag = true;
+}
